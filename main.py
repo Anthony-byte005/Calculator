1cf9d1a75053d09a9c0b2fdf50d7f1c43f317ba6
snx2 <lunguj665@gmail.com>
2025-11-04 23:59:48 +0200

    added a table to the results

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import typer
 from rich import print
+from rich.console import Console
+from rich.table import Table
 import utils.operators as operators  # <- clearer alias
 
 app = typer.Typer()
-
+console = Console()
 @app.command()
 def version():
     print("xyrculator version [bold red]pre:[green]0.1")
-
+#multiplication sub command
 @app.command()
 def multiply(a: int, b: int):
-    print(operators.multiply(a, b))
+    table = Table("operation", "answer")
+    table.add_row(f"{str(a)} × {str(b)}", str(operators.multiply(a,b)))
+    console.print(table)
+
+#division sub command
 @app.command()
 def divide(a: int, b:int):
-    print(operators.divide(a,b))
+    table = Table("operation", "answer")
+    table.add_row(f"{str(a)}÷{str(b)}", str(operators.divide(a,b)))
+    console.print(table)
 if __name__ == "__main__":
     app()
